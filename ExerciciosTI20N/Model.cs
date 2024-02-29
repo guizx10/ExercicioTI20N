73c9ff55a72f6dceed444376799e33757ce16633
GUILHERME FERREIRA DOS SANTOS <guilherme.fsantos62@senacsp.edu.br>
2024-02-28 22:17:24 -0300

    Primeiro Commit

---
+++ ExerciciosTI20N/Model.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosTI20N
+{
+    class Model
+    {
+        //Construtor
+        public Model()
+        {
+
+
+        }//fim do metodo construtor 
+
+        //Exercicio 01: Faça um programa que imprima os números de 1 ao 10. 
+        public string ExercicioUm()
+        {
+            string resultado = ""; 
+
+            for(int i=0; i< 10; i++)
+            {
+
+                resultado += "\n" + i+1;
+
+            }
+
+        }//fim do Método 
+
+    }//fim da classe
+}//fim do projeto 
